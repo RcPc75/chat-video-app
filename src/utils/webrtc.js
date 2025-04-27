77eef5e0cd71a33f363dd764752372e7dd6f6a55
RcPc75 <rcpc.rcpc1@gmail.com>
2025-04-27 20:48:45 +0500

    Remove unused localStream to fix Vercel deployment

--- src/utils/webrtc.js
+++ src/utils/webrtc.js
@@ -8,7 +8,7 @@ export function startWebRTC({
   setLocalStream,
 }) {
   let pc
-  let localStream
+  // let localStream
 
   navigator.mediaDevices.getUserMedia({ video: true, audio: true })
     .then(stream => {
@@ -17,7 +17,7 @@ export function startWebRTC({
       if (setLocalStream) setLocalStream(stream)
 
       localVideo.current.srcObject = stream
-      localStream = stream
+      // localStream = stream
 
       pc = new RTCPeerConnection({
         iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
